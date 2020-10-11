51b14ee5571203b930830b0a975f3b4f7c144fdb
lqkhoo <changtau2005@gmail.com>
2020-10-12 06:16:21 +0800

    HW3 P1 complete

--- hw3/AA274A_HW3/Problem_1/cam_calibrator.py
+++ hw3/AA274A_HW3/Problem_1/cam_calibrator.py
@@ -64,13 +64,48 @@ class CameraCalibrator:
             u_meas: a list of arrays where each array are the u values for each board.
             v_meas: a list of arrays where each array are the v values for each board.
         Output:
-            corner_coordinates: a tuple (Xg, Yg) where Xg/Yg is a list of arrays where each array are the x/y values for each board.
+            corner_coordinates: a tuple (Xg, Yg) where Xg/Yg is a list of arrays where
+                                each array are the x/y values for each board.
 
         HINT: u_meas, v_meas starts at the blue end, and finishes with the pink end
         HINT: our solution does not use the u_meas and v_meas values
         HINT: it does not matter where your frame it, as long as you are consistent!
         '''
         ########## Code starts here ##########
+
+        # u_meas, v_meas returns the corners in pixel coordinates, where
+        # bottom-left is (0,0). Points are ordered from blue to pink.
+        # Slanted lines show where the start of the next row is.
+
+        # This is all good, but 
+        # the Pset wants World coordinates (X,Y) attached to the
+        # checkerboards, so we just generate these accordingly
+        # based on what we know about the checkerboards.
+
+        # In world coordinates (X,Y) in meters. The world reference frame
+        # is centered on our arbitrary object. The way we're generating it,
+        # X-axis goes from left-> right
+        # Y-axis goes from top -> down
+        # because that's how the points in u/v_meas are ordered in the array.
+
+        N = self.n_chessboards
+        nx, ny = self.n_corners_x, self.n_corners_y
+        d = self.d_square
+        # u_meas, v_meas have shapes (N, nx*ny)
+
+        X_list = []
+        Y_list = []
+
+        # We don't assume nx, ny to be constant, so we need a loop
+        for _ in range(N):
+            Xs = np.multiply(d, np.arange(nx))
+            Xs = np.tile(Xs, ny)    # Shape (nx*ny,) 1,2,3,4,1,2,3,4,1,2,3,4,...
+            Ys = np.multiply(d, np.arange(ny))
+            Ys = np.repeat(Ys, nx)  # Shape (nx*ny,) 1,1,1,2,2,2,3,3,3,4,4,4,...
+            X_list.append(Xs)
+            Y_list.append(Ys)
+
+        corner_coordinates = (X_list, Y_list)
 
         ########## Code ends here ##########
         return corner_coordinates
@@ -86,13 +121,71 @@ class CameraCalibrator:
             H: the homography matrix. its size is 3x3
 
         HINT: What is the size of the matrix L?
-        HINT: What are the outputs of the np.linalg.svd function? Based on this, where does the eigenvector corresponding to the smallest eigen value live?
+        HINT: What are the outputs of the np.linalg.svd function? Based on this,
+                where does the eigenvector corresponding to the smallest eigen value live?
         HINT: np.stack and/or np.hstack may come in handy here.
         '''
         ########## Code starts here ##########
 
+        # Zhang 1998. See also lecture 9 notes, especially section 9.2.1
+        # Lecture 9 (2019) 54 minute mark onwards (Step 1)
+        # Equation number (x) refer to lecture 9 notes.
+        # We follow the notation in the lecture notes.
+
+        # We need to reconstruct the homography matrix M mapping (homogeneous) world
+        # coordinates to pixel coordinates.
+
+        nx, ny = self.n_corners_x, self.n_corners_y
+        u, v = u_meas, v_meas # shapes (nx*ny, )
+        # X, Y as given, Z = 0 as we know the points are co-planar.
+
+        P_W = np.array([X, Y, np.ones(nx*ny)]) # shape (k, nx*ny) = (3, 63)
+        k = P_W.shape[0]
+
+        # P_tilde has shape (2*nx*ny, 3k)
+        # Each 'block' is [-1  0 ui ] * P_{W,i}^T
+        #                 [ 0 -1 vi ]
+        # and there are N of these blocks as we have N points per image.
+        # P_{W,i} is a col vector of length 3.
+
+        # This is not strictly matrix P_tilde as written in eq (18).
+        # We blocked all rows with u and all the rows with v together.
+        # Since each row is just a different constraint, this works just fine for SVD.
+        P_tilde = np.block([
+            [-1*P_W.T,  0*P_W.T, (u*P_W).T],
+            [ 0*P_W.T, -1*P_W.T, (v*P_W).T]
+        ])
+
+        U, s, VT = np.linalg.svd(P_tilde, full_matrices=False) # We don't need padding.
+        # Eigenvalues s sorted from largest to smallest.
+        # We want the eigenvectors of P.T dot P, which corrresponds to V transpose.
+        # Grab the smallest one in the last row, and reshape to M, which is 3x3.
+        M = np.reshape(VT[-1, :], (k,k))
+
+        H = M # Convert back to Zhang's terminology.
+
         ########## Code ends here ##########
         return H
+
+    def compute_Vij(self, H_T, i, j):
+        """
+        Helper for getCameraIntrinsics.
+        Expects H to already be transposed.
+        arguments i and j should be cardinal (1-indexed)
+        """
+        i, j = i-1, j-1 # Translate to ordinal array index
+        Hi, Hj = H_T[i], H_T[j]
+
+        V_ij = np.array([
+            Hi[0] * Hj[0],
+            Hi[0] * Hj[1] + Hi[1] * Hj[0],
+            Hi[1] * Hj[1],
+            Hi[2] * Hj[0] + Hi[0] * Hj[2],
+            Hi[2] * Hj[1] + Hi[1] * Hj[2],
+            Hi[2] * Hj[2]
+        ])
+
+        return V_ij
 
     def getCameraIntrinsics(self, H):    # Zhang 3.1, Appendix B
         '''
@@ -108,6 +201,61 @@ class CameraCalibrator:
         '''
         ########## Code starts here ##########
 
+        # Equation numbers (x) refer to lecture 9 notes.
+
+        H_list = H # Rebind variable name
+        N = self.n_chessboards
+        k = 3
+
+        # Likewise, we will not replicate the matrix V exactly as in eq (27)
+        # since it's just rows of SVD constraints.
+
+        # First let's get rid of the list. (why use a list ?)
+        # Each H is a 3x3, so let's turn the list into a tensor of shape (3, 3, N),
+        # such that H[i, j] has shape (N, ).
+        H_T = np.zeros((k, k, N))
+        for n in range(N):
+            H_T[:,:,n] = H_list[n].T # Note the transpose just under eq (26)
+
+        V_11 = self.compute_Vij(H_T, 1, 1) # shape (2k, N) = (6, 23)
+        V_12 = self.compute_Vij(H_T, 1, 2)
+        V_22 = self.compute_Vij(H_T, 2, 2)
+
+        V = np.vstack((
+            V_12.T,
+            (V_11-V_22).T
+        )) # shape (2N, 2k) = (46, 6)
+
+        U, s, VT_ = np.linalg.svd(V, full_matrices=False)
+        b = VT_[-1, :]
+
+        # We don't actually end up using the matrix B.
+        # B = np.array([ # This is symmetric. We don't need the lower triangle.
+        #     [b[0], b[1], b[3]],
+        #     [b[1], b[2], b[4]],
+        #     [b[3], b[4], b[5]]
+        # ])
+
+        # Then just plug in eq (30)
+
+        (B11, B12, B22, B13, B23, B33) = b
+
+        t1 = B12*B13 - B11*B23
+        t2 = B11*B22 - B12*B12
+
+        v0  = t1 / t2
+        lmb = B33 - (B13*B13 + v0*t1) / B11
+        alp = np.sqrt(lmb / B11)
+        bet = np.sqrt(lmb * B11 / t2)
+        gam = -B12 * alp*alp * bet / lmb
+        u0  = gam * v0 / bet - B13 * alp*alp / lmb
+
+        A = np.array([
+            [alp, gam, u0],
+            [0  , bet, v0],
+            [0  , 0  , 1 ]
+        ])
+
         ########## Code ends here ##########
         return A
 
@@ -121,6 +269,23 @@ class CameraCalibrator:
             t: the translation vector
         '''
         ########## Code starts here ##########
+
+        # Just follow equation (31) of lecture 9 notes
+        A_inv = np.linalg.inv(A)
+        c1, c2, c3 = H[:,0], H[:,1], H[:,2] # The cs are columns of H
+
+        q = 1 / np.linalg.norm(np.dot(A_inv, c1)) # common recurring term
+        r1 = q * np.dot(A_inv, c1)
+        r2 = q * np.dot(A_inv, c2)
+        r3 = np.cross(r1, r2)
+        t  = q * np.dot(A_inv, c3)
+
+        R = np.column_stack((r1, r2, r3)) # The rs are columns of R.
+
+        U, s, VT = np.linalg.svd(R, full_matrices=False)
+        R_sol = np.dot(U, VT)
+        
+        R = R_sol        
 
         ########## Code ends here ##########
         return R, t
@@ -136,6 +301,16 @@ class CameraCalibrator:
 
         '''
         ########## Code starts here ##########
+        
+        d = X.shape[0]
+
+        P_Wh = np.array([X, Y, Z, np.ones(d)])  # shape (4, 63) # Homogeneous world coords
+        Rt = np.column_stack((R,t))             # shape (3, 4)
+        P_Ch = np.dot(Rt, P_Wh)                 # shape (3, 63) # Homogeneous camera coords
+
+        # Transform back to nonhomogeneous coords
+        P_Ch = P_Ch / P_Ch[2]
+        x, y, _ = P_Ch
 
         ########## Code ends here ##########
         return x, y
@@ -152,6 +327,17 @@ class CameraCalibrator:
         '''
         ########## Code starts here ##########
 
+        d = X.shape[0]
+
+        P_Wh = np.array([X, Y, Z, np.ones(d)])  # shape (4, 63) # Homogeneous world coords
+        Rt = np.column_stack((R,t))             # shape (3, 4)
+        P_Ch = np.matmul(Rt, P_Wh)              # shape (3, 63) # Homogeneous camera coords
+        ph = np.matmul(A, P_Ch)                 # shape (3, 63) # Homogeneous pixel coords
+
+        # Transform to nonhomogeneous coords
+        ph = ph / ph[2]
+        u, v, _ = ph
+        
         ########## Code ends here ##########
         return u, v
 
@@ -360,6 +546,7 @@ class CameraCalibrator:
         v_meas = []
         for chessboards in self.c.good_corners:
             u_meas.append(chessboards[0][:, 0][:, 0])
+            # v_meas.append(chessboards[0][:, 0][:, 1]) # TODO this keeps Y's direction.
             v_meas.append(self.h_pixels - chessboards[0][:, 0][:, 1])   # Flip Y-axis to traditional direction
 
         return u_meas, v_meas   # Lists of arrays (one per chessboard)
